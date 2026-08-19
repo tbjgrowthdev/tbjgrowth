67bf2ae09705446ffbbe178ffd21ef665396d38d
asif938 <asif1962000@gmail.com>
2026-08-19 17:44:45 +0600

    added pg

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.join(__dirname),
   },
+  serverExternalPackages: ["pg", "pg-cloudflare"],
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
