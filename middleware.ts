10605f0eb9b4fa0da9fe0ac208a03083a4c293b0
asif938 <asif1962000@gmail.com>
2026-08-19 17:34:29 +0600

    fixed deploy loop

--- middleware.ts
+++ middleware.ts
@@ -68,4 +68,5 @@ export default withAuth(
 
 export const config = {
   matcher: ["/((?!_next/static|_next/image|favicon.ico|api/).*)"],
+  runtime: "experimental-edge",
 };
--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,6 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
+export default {
+  ...defineCloudflareConfig(),
   buildCommand: "npx prisma generate && npx next build",
-});
+};
